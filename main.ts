9c81e22967d7c788a439a42e7ea079fea639f8b8
Tim Riscalas <61475187+s454835@users.noreply.github.com>
2021-04-01 16:06:58 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let angle = 0
+let opened = 95
+let closed = 175
+basic.forever(function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    0
+    )
+    pins.servoWritePin(AnalogPin.P0, input.lightLevel())
+})
+basic.forever(function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    0
+    )
+    angle = pins.map(
+    input.lightLevel(),
+    0,
+    255,
+    opened,
+    closed
+    )
+    pins.servoWritePin(AnalogPin.P0, angle)
+})

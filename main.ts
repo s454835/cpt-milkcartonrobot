cf18f248f7fd16779879d5c3fc44875f41dfb251
Tim Riscalas <61475187+s454835@users.noreply.github.com>
2021-04-06 13:07:40 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Created by Tim")
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("E B C5 A B G A F ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    current_variable = input.temperature()
+    basic.showNumber(input.temperature())
+    basic.pause(100)
+    basic.clearScreen()
+})
 let angle = 0
+let current_variable = 0
 let opened = 95
 let closed = 175
 basic.forever(function () {
